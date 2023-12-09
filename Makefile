8ff588f46ce227f9a7bd19ebcf8e2dc4772ba5f4
melp <huzi.master@outlook.com>
2023-12-09 13:24:49 +0800

    publish

--- Makefile
+++ Makefile
@@ -6,5 +6,11 @@ clean:
 	rm -fr public
 
 .PHONY: deploy
+# deploy: public/index.xml
+# 	rsync -avz --delete public/* defn:~/www
+
 deploy: public/index.xml
-	rsync -avz --delete public/* defn:~/www
+	cp -r public/* ../evalwhen.github.io
+	cd ../evalwhen.github.io
+	git commit -am "publish"
+	git push
--- pages/about.md.rkt
+++ pages/about.md.rkt
@@ -10,8 +10,8 @@ I acquired programming skills through self-study. I commenced learning programmi
 SICP is my first programming book, i loved it, enjoy reading it, i learned so much from SICP.
 
 In my free time, I enjoy coding in scheme. In this blog, I will write things about Scheme and Computation.
-I will show you Scheme is alovely language. It's also a serious language, little core and
-based on macro, we can implement so many common features in other mainstream languages.
+I will show you Scheme is alovely language. It's also a serious language and has little core,
+By leveraging macros, we can implement so many common language features.
 
 I'll predominantly use English to share my think with the world, thank you.
 
